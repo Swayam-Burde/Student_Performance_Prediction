37223820f0eb1d81f0698f176c83c30dca22eea1
Swayam Burde <swayamburde2004@gmail.com>
2025-07-27 16:21:04 +0530

    Data Trandformation added

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 
 def error_message_details(error, error_detail):
     _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_fileame
+    file_name = exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
     return error_message
 
